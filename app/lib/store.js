4aa9b61dca7ca4a13558a6da207a56efed743184
SteevenSF01 <salgadosteeven@gmail.com>
2024-03-27 16:00:44 +0100

    sign in component

--- app/lib/store.js
+++ app/lib/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterSlice from '@/app/lib/features/counter/counterSlice'
 import themeSlice from '@/app/lib/features/theme/theme'
 
 const  store = configureStore({
     devTools: true,
     reducer: {
-        counter: counterSlice,
         theme: themeSlice,
     },
 });
