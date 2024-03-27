bc2f24dec4b72c53493092ed567d8b291c6de469
SteevenSF01 <salgadosteeven@gmail.com>
2024-03-27 12:30:13 +0100

    fin de homepage

--- app/page.js
+++ app/page.js
@@ -1,8 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <main className="flex flex-col h-full justify-around items-center">
+      <div className="flex flex-col items-center text-2xl font-semibold">
+        <Image
+          src="/images/imgacceuil.jpg"
+          alt="Profile Picture"
+          width={1000}
+          height={720}
+          className="w-80"
+        />
+        <h1>Readit</h1>
+      </div>
+      <Link href={"/login"}>
+        <button className="bg-[#F01D19] text-white px-28 py-4 rounded-xl">
+          Explore
+        </button>
+      </Link>
     </main>
   );
 }
