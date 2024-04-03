15e8e896d9bed34c5d29fc0d0bc7763cfb9f77d0
SteevenSF01 <salgadosteeven@gmail.com>
2024-04-03 09:58:27 +0200

    ajout du hover dans bestsellers

--- app/components/bestSellers/BestSellers.jsx
+++ app/components/bestSellers/BestSellers.jsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import Link from "next/link";
+import { roboto } from "@/app/fonts";
 
-export default function BestSellers({books}) {
-
+export default function BestSellers({ books }) {
   return (
     <>
       <section className="pt-5 pb-10">
@@ -9,7 +10,7 @@ export default function BestSellers({books}) {
           <div className="w-fit flex h-[350px] rounded-xl bg-[#323232] gap-x-5 items-center px-5 ">
             {books.map((book, i) => {
               return (
-                <div className="bg-lime-200 w-72 h-80 " key={i}>
+                <div className="relative w-72 h-80 " key={i}>
                   <Image
                     className="w-[400px] h-full object-fill"
                     src={book.image_url}
@@ -17,6 +18,22 @@ export default function BestSellers({books}) {
                     height={300}
                     alt={book.title}
                   />
+                  <div className="w-full h-full bg-gradient-to-b from-slate-900 from-10% via-black via-75% to-black absolute top-0 right-0 opacity-0 hover:opacity-90 transition-all duration-300 flex flex-col justify-center items-center p-5 overflow-auto scrollBar-thumb">
+                    <div className={`flex flex-col justify-center items-center w-[85%] h-full gap-y-5 ${roboto.className}`}>
+                      <p className="text-white text-center ">Title : {book.title}</p>
+                      <p className="text-white text-center "> Authors : {book.authors}</p>
+                      <p className="text-white ">Rating : {book.rating}</p>
+                      <p className="text-white ">{book.num_pages} Pages</p>
+                    </div>
+                    <Link
+                      href={`/home/details/${book.id - 1}`}
+                      className="flex justify-center"
+                    >
+                      <button className="bg-[#E00404] my-5 py-1 px-6 hover:bg-[#e00404ac] rounded-xl ">
+                        More Details
+                      </button>
+                    </Link>
+                  </div>
                 </div>
               );
             })}
